9ad6b5fe9d43f86291c8b0fece87b2cd4276c7e5
Owen McConville <owen.mcconville21@gmail.com>
2023-01-12 13:04:07 +0000

    first commit

---
+++ src/main/java/com/allstate/claimsfirstserver/ClaimsFirstServerApplication.java
@@ -0,0 +1,13 @@
+package com.allstate.claimsfirstserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimsFirstServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaimsFirstServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allstate/claimsfirstserver/ClaimsFirstServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allstate.claimsfirstserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaimsFirstServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
